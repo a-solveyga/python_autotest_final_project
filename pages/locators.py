8e3a95dcb3922ee36a53ce0c9cd2d3683bf636f5
Solveyga Azbite <solveyga.azbite@semrush.com>
2021-05-29 19:05:08 +0300

    move login link logic to base class

--- pages/locators.py
+++ pages/locators.py
@@ -1,8 +1,13 @@
 from selenium.webdriver.common.by import By
 
 
-class MainPageLocators():
+class BasePageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+
+
+# class MainPageLocators():
+   # LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 
 
 class LoginPageLocators():
@@ -10,9 +15,10 @@ class LoginPageLocators():
     REGISTRATION_FORM = (By.CSS_SELECTOR, "#register_form")
 
 
-class ProductPageLocator():
+class ProductPageLocators():
     ADD_TO_BASKET_BUTTON = (By.CSS_SELECTOR, "button.btn-add-to-basket")
-    BASKET_ALERT = (By.CSS_SELECTOR, "#messages .alert:nth-child(1) strong")
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, "#messages .alert:nth-child(1) strong")
     PRODUCT_NAME = (By.CSS_SELECTOR, ".col-sm-6.product_main h1")
     BASKET_AMOUNT = (By.CSS_SELECTOR, "#messages .alert:nth-child(3) strong")
     PRODUCT_AMOUNT = (By.CSS_SELECTOR, ".col-sm-6.product_main .price_color")
+    # SUCCESS_MESSAGE = (By.CSS_SELECTOR, "#messages .alert:nth-child(1) strong") #"#message .alert"
--- pages/product_page.py
+++ pages/product_page.py
@@ -1,10 +1,10 @@
 from .base_page import BasePage
-from .locators import ProductPageLocator
+from .locators import ProductPageLocators
 
 
 class ProductPage(BasePage):
     def add_to_basket(self):
-        add_basket_button = self.browser.find_element(*ProductPageLocator.ADD_TO_BASKET_BUTTON)
+        add_basket_button = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_BUTTON)
         add_basket_button.click()
 
     def should_be_item_in_basket(self):
@@ -12,13 +12,20 @@ class ProductPage(BasePage):
         self.should_be_price_message()
 
     def should_be_adding_message(self):
-        product_title_basket = self.browser.find_element(*ProductPageLocator.BASKET_ALERT).text
-        product_title_page = self.browser.find_element(*ProductPageLocator.PRODUCT_NAME).text
-        assert product_title_basket == product_title_page, "Message about item in basket doesn't fit with product price."
+        product_title_basket = self.browser.find_element(*ProductPageLocators.SUCCESS_MESSAGE).text
+        product_title_page = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
+        assert product_title_basket == product_title_page, \
+            "Message about item in basket doesn't fit with product price."
 
     def should_be_price_message(self):
-        product_price_basket = self.browser.find_element(*ProductPageLocator.BASKET_AMOUNT).text
-        product_price_page = self.browser.find_element(*ProductPageLocator.PRODUCT_AMOUNT).text
+        product_price_basket = self.browser.find_element(*ProductPageLocators.BASKET_AMOUNT).text
+        product_price_page = self.browser.find_element(*ProductPageLocators.PRODUCT_AMOUNT).text
         assert product_price_basket == product_price_page, "Message about price doesn't fit with product price."
 
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is presented, but should not be"
 
+    def should_be_disappeared_success_message(self):
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is yet presented, but should be disappeared"
